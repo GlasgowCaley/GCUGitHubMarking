56430c119e7b29fb71c105b320dc522d1d3cf27b
Brian McDonald <brian.mcdonald@gcu.ac.uk>
2012-09-20 23:31:43 +0100

    Displaying a list of the names of repos

--- js/githubmarking.js
+++ js/githubmarking.js
@@ -9,50 +9,63 @@ var timeout=0;
 var baseURL="https://api.github.com/";
 var userURL="users/"
 
-var viewModel={
-    searchTerm: ko.observable(),
-    searchTypes:ko.observableArray(['User', 'Team', 'Organisation']),
-    selectedSearchType : ko.observable('User'),
-    user: ko.observable(),
-    team: ko.observable(),
-    repo: ko.observableArray([]),
+function ViewModel(){
+    var self = this;
 
-    search: function(){
-        if (this.searchTerm().length>3)
+    self.searchTerm=ko.observable();
+    self.searchTypes=ko.observableArray(['User', 'Team', 'Organisation']);
+    self.selectedSearchType=ko.observable('User');
+    self.user=ko.observable();
+    self.team=ko.observable();
+    self.repo=ko.observableArray([]);
+
+    self.search=function(){
+        if (self.searchTerm().length>3)
         {
             //timeout
             clearTimeout(timeout);
             timeout=setTimeout(function(){
-                carryOutGitHubSearch(viewModel.selectedSearchType(),viewModel.searchTerm());
+                carryOutGitHubSearch(self.selectedSearchType(),self.searchTerm());
             },500);
 
 
         }
+    },
+
+    self.logModel=function()
+    {
+        console.log("Search Term "+self.searchTerm());
+        console.log("Selected Search "+self.selectedSearchType());
+        console.log("User "+self.user.name());
+        //iterate through repos
     }
+
 };
 
 $(function(){
     infuser.defaults.templateSuffix = ".tmpl.html";
     infuser.defaults.templateUrl = "templates";
-    ko.applyBindings(viewModel);
+    window.vm=new ViewModel();
+    ko.applyBindings(vm);
+    window.vm.selectedSearchType.subscribe(function(newValue) {
+        carryOutGitHubSearch(newValue,window.vm.searchTerm());
+    });
 });
 
-viewModel.selectedSearchType.subscribe(function(newValue) {
-    carryOutGitHubSearch(newValue,viewModel.searchTerm());
-});
+
 
 function getRepos()
 {
     clearTimeout(timeout);
     timeout=setTimeout(function()
     {
-        retrieveUsersGitHubRepos(viewModel.searchTerm());
+        retrieveUsersGitHubRepos(window.vm.searchTerm());
     },500);
 }
 
 function retrieveUsersGitHubRepos(searchTerm)
 {
-    if(viewModel.searchTerm()!=null){
+    if(window.vm.searchTerm()!=null){
         //console.log(searchType+" "+searchTerm);
 
         ///users/:user/repos
@@ -76,14 +89,15 @@ function retrieveUsersGitHubRepos(searchTerm)
             dataType: 'jsonp',
             success: null,
             error:null,
-            traditional:true
+            traditional:true,
+            crossDomain:true
         });
     }
 }
 
 function carryOutGitHubSearch(searchType,searchTerm)
 {
-    if(viewModel.searchTerm()!=null){
+    if(window.vm.searchTerm()!=null){
         console.log(searchType+" "+searchTerm);
         var url=baseURL+userURL+searchTerm;
         //var url="https://api.github.com?callback=foo&Origin=http://www.scottishcodemonkey.com";
@@ -104,25 +118,28 @@ function carryOutGitHubSearch(searchType,searchTerm)
             contentType: "application/json",
             dataType: 'jsonp',
             success: null,
-            error:null
+            error:null,
+            crossDomain:true
+
         });
     }
 }
 
 function searchCallback(response) {
     var meta = response.meta;
-    viewModel.user=ko.mapping.fromJS(response.data);
-    console.log(meta);
-    console.log(response.data);
-    ko.applyBindings(viewModel);
+    window.vm.user=ko.mapping.fromJS(response.data);
+    //console.log(meta);
+    //console.log(response.data);
+    ko.applyBindings(window.vm);
+    window.vm.logModel();
 }
 
 //http://jsfiddle.net/rniemeyer/tD4pH/
 function listReposCallback(response)
 {
     var meta = response.meta;
-    viewModel.repo=ko.mapping.fromJS(response.data);
-    console.log(meta);
-    ko.applyBindings(viewModel);
-    console.log(viewModel.repo()[0].name());
+    window.vm.repo=ko.mapping.fromJS(response.data);
+    //console.log(meta);
+    ko.applyBindings(window.vm);
+    window.vm.logModel();
 }
